1f40e2a4241dc946efae65e5a7d90aff6b5f9a2b
심동목 <simdongmok@macbook-pro-4.local>
2019-01-07 16:04:56 +0900

    Initial Commit

---
+++ pra_2019_02_07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pra_2019_02_07
+//
+//  Created by 심동목 on 2019/01/07.
+//  Copyright © 2019 シムどんモク. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
